20e69c88a99cbd8e5306f09c6aa344ae94984bec
Brent Gauthier <cajundrummer@gmail.com>
2024-03-19 18:29:28 -0400

    Build App page and set up routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from'react-router-dom';
+// import Navigation from './Components/Navigation';
+// import Landing from './Pages/Landing';
+// import About from './Pages/About';
+// import Blog from './Pages/Blog';
+// import Books from './Pages/Books';
+// import Contact from './Pages/Contact';
+// import Footer from './Components/Footer';
+// import Press from './Pages/Press';
+// import PostDetails from './Pages/PostDetails';
+// import './Styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className = 'App'>
+      <div className = 'container'>
+        <BrowserRouter>
+          <div className = 'Navigation'>
+            {/* <Navigation /> */}
+          </div>
+          <div className = 'content'>
+            <Routes>
+              {/* <Route path = '/' element = {<Landing />} /> */}
+              {/* <Route path = '/about' element = {<About />} /> */}
+              {/* <Route exact path = '/blog' element = {<Blog />} /> */}
+              {/* <Route exact path = '/blog/posts/:slug' element = {<PostDetails />} /> */}
+              {/* <Route exact path = '/books' element = {<Books />} /> */}
+              {/* <Route path = '/contact' element = {<Contact />} /> */}
+              {/* <Route path = '/press' element = {<Press />} /> */}
+            </Routes>
+          </div>
+          {/* <Footer /> */}
+
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
