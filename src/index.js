8123f0c2b62350a61e337a052f364f86a4bcd41f
Brent Gauthier <cajundrummer@gmail.com>
2024-03-19 18:30:07 -0400

    Remove conflict with missing component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import '../src/Styles/Globals.scss';
+// import '../src/Styles/Globals.scss';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from 'apollo-link-context';
 
